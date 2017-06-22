e4da379295af46cdce5f805beadadb031d9d87e9
Mikael23 <mpj2310@gmail.com>
2017-06-22 19:51:37 +0300

    NewShop

---
+++ src/Shop.java
@@ -0,0 +1,60 @@
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("serial")
+public class Shop implements Serializable {
+
+	 Map<Integer,Goods> goods = new HashMap<>();
+	
+
+	
+	
+	
+	   public Goods addgoods(Goods product){
+		
+		
+	    
+		
+		
+		
+		return goods.put(product.id, product);
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Shop [goods=" + goods + ", name=" + name + ", totalFund=" + totalFund + "]";
+	}
+
+
+
+	public String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+
+	public int getTotalFund() {
+		return totalFund;
+	}
+
+	public void setTotalFund(int totalFund) {
+		this.totalFund = totalFund;
+	}
+
+	
+	
+	
+	public int totalFund;
+	
+}
+
+
