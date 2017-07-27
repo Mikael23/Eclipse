4a6379a5237d963013483b0b2dcd87a047181cf0
Mikael23 <mpj2310@gmail.com>
2017-07-27 19:44:29 +0300

    demo

---
+++ demoo.java
@@ -0,0 +1,104 @@
+import java.awt.print.Book;
+import java.lang.reflect.Array;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class demoo implements Runnable {
+	private final static ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+	private final static Lock readLock = readWriteLock.readLock();
+
+	private final static Lock writeLock = readWriteLock.writeLock();
+	static int[] book = new int[5];
+	static Random random = new Random();
+
+	public static void main(String[] args) throws Exception {
+		demoo rt = new demoo();
+		ExecutorService ex = Executors.newFixedThreadPool(3);
+
+		Runnable task = new Runnable() {
+			@Override
+			public void run() {
+
+				try {
+					nextPage();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			};
+		
+			
+			Runnable task1 = new Runnable(){
+
+				@Override
+				public void run() {
+					try{
+						read();
+					}catch (InterruptedException e) {
+						e.printStackTrace();
+					
+				}
+				
+			}
+			};
+		
+		
+		ex.submit(task1);
+		ex.submit(task1);
+		ex.submit(task);
+		ex.shutdown();
+		ex.shutdown();
+		ex.shutdown();
+	}
+	
+	public static Runnable read() throws InterruptedException {
+		readLock.lock();
+		try {
+			for (int i = 0; i <= book.length; i++) {
+				System.out.println(book[i]);
+				System.out.println(Thread.currentThread().getName());
+
+			}
+			Thread.sleep(1000);
+		} finally {
+			readLock.unlock();
+		}
+		return null;
+
+	}
+
+	public static void nextPage() throws InterruptedException {
+		writeLock.lock();
+		try {
+			System.out.println("nextPage start");
+			for (int i = 0; i < book.length - 1; i++) {
+
+				book[i] = random.nextInt(5);
+			}
+			Thread.sleep(5000);
+			System.out.println("nextPage finish");
+
+		} finally {
+			writeLock.unlock();
+		}
+
+	}
+
+	@Override
+	public void run() {
+
+		try {
+			read();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
